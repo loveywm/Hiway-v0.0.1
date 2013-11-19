a228612e496b7a844d24540d1309f12eb1b03e26
loveywm <490054348@qq.com>
2013-11-19 11:34:36 +0800

    add 主程序控制q

---
+++ hiway.pro
@@ -0,0 +1,25 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-09-27T20:13:07
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET =
+TEMPLATE =
+
+
+SOURCES += \
+    main.cpp
+
+
+
+HEADERS  +=
+
+
+FORMS    +=
+
+RESOURCES +=
---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+//#include "cmainframe.h"
+#include <QApplication>
+//#include "../gui/menu.h"
+//#include "Define.h"
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+
+    //CMainFrame w;
+    //w.show();
+
+    //menu m;
+    //m.show();
+
+
+    //CMenu m;
+    //m.show();
+    //m.MenuProc(UISTR_MENU_MAINMENU);
+
+
+    return a.exec();
+}
